d3877dc7ca16b8a56c05b64f7a9fe62671f8ec0c
l3miage-grasiaa <Angelica-Rivera.Grasia@etu.univ-grenoble-alpes.fr>
2024-03-25 16:42:03 +0100

    suppression des @column non obligatoire ...

--- server/src/main/java/fr/uga/l3miage/tp4/models/CandidateEvaluationGridEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp4/models/CandidateEvaluationGridEntity.java
@@ -6,12 +6,9 @@ import java.time.LocalDateTime;
 @Entity
 public class CandidateEvaluationGridEntity {
     @Id
-    @Column(name="sheet_number")
     private Long sheetNumber;
 
-    @Column(name="grade")
     private double grade;
 
-    @Column(name="submission_date")
     private LocalDateTime submissionDate;
 }
