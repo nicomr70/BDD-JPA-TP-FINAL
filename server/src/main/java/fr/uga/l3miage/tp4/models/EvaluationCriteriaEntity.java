c8ffb419f924091cfb3e190e72fad0cf7757bbab
l3miage-grasiaa <Angelica-Rivera.Grasia@etu.univ-grenoble-alpes.fr>
2024-03-25 16:45:00 +0100

    suppression des @column non obligatoire dans le fichier EvaluationCriteriaEntity.java

--- server/src/main/java/fr/uga/l3miage/tp4/models/EvaluationCriteriaEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp4/models/EvaluationCriteriaEntity.java
@@ -5,12 +5,10 @@ import javax.persistence.*;
 @Entity
 public class EvaluationCriteriaEntity {
     @Id
-    @Column(name="id")
     private Long id;
 
-    @Column(name="description", nullable=false)
+    @Column(nullable=false)
     private String description;
 
-    @Column(name="has_candidate_performed_criteria")
     private boolean hasCandidatePerformedCriteria;
 }
