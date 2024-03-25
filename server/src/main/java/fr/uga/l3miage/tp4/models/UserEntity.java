a004741fc9677d7db6b0fe5fb16a0884f17a87b2
Yacineos <psmartyacine@gmail.com>
2024-03-25 15:54:58 +0100

    UserEntity

--- server/src/main/java/fr/uga/l3miage/tp4/models/UserEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp4/models/UserEntity.java
@@ -1,4 +1,23 @@
 package fr.uga.l3miage.tp4.models;
 
-public class UserEntity {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class UserEntity {
+
+    @Id
+    private Long id ;
+
+    private String firstname ;
+    private String lastname ;
+    @Column(unique = true, nullable = false)
+    private String email ;
+    @Column(unique = true)
+    private String phonenumber ;
 }
