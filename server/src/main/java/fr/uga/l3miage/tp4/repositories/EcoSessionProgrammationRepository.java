4bd838cfb0f331ae8d5bd50138f641af46b9922b
l3miage-lawriwe <elawriw@gmail.com>
2024-03-25 17:04:01 +0100

    début d'essai de named function

--- server/src/main/java/fr/uga/l3miage/tp4/repositories/EcoSessionProgrammationRepository.java
+++ server/src/main/java/fr/uga/l3miage/tp4/repositories/EcoSessionProgrammationRepository.java
@@ -2,10 +2,14 @@ package fr.uga.l3miage.tp4.repositories;
 
 import fr.uga.l3miage.tp4.models.EcoSessionEntity;
 import fr.uga.l3miage.tp4.models.EcoSessionProgrammationEntity;
+import fr.uga.l3miage.tp4.models.EcoSessionProgrammationStepEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface EcoSessionProgrammationRepository extends JpaRepository<EcoSessionProgrammationEntity,String> {
+
+    //debut d'éssaide named function
+    //EcoSessionProgrammationStepRepository findFirstByAPourEtapesIsAfter();
 }
