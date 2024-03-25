0e4cddc6854088536f857e4526f3260dfb46f8d4
l3miage-gentildr <driss.gentil@etu.univ-grenoble-alpes.fr>
2024-03-25 17:07:35 +0100

    Added UserEntity

---
+++ server/src/main/java/fr/uga/l3miage/tp4/models/UserEntity.java
@@ -0,0 +1,19 @@
+package fr.uga.l3miage.tp4.models;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class UserEntity {
+    @Id
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @Column(unique = true)
+    private String phoneNumber;
+}
