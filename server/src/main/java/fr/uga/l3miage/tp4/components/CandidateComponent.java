7f5cc1b3abc23f28e2d33aba1da327e0a59c127c
l3miage-seyfa <seyfa@etu.univ-grenoble-alpes.fr>
2024-03-26 23:59:40 +0100

    Q2 et Q4 final

--- server/src/main/java/fr/uga/l3miage/tp4/components/CandidateComponent.java
+++ server/src/main/java/fr/uga/l3miage/tp4/components/CandidateComponent.java
@@ -1,14 +1,24 @@
 package fr.uga.l3miage.tp4.components;
 
+import fr.uga.l3miage.tp4.models.CandidateEntity;
+
 import fr.uga.l3miage.tp4.repositories.CandidateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.time.LocalDate;
 
 @Component
 @RequiredArgsConstructor
 public class CandidateComponent {
 
     private CandidateRepository candidateRepository;
+
+    /*avant le 01/01/2000 */
+    public Set<CandidateEntity> getAllWithoutExtraTimeWithBirthDay(){
+        return candidateRepository.findByHasExtraTimeFalseAndBirthDateBefore(LocalDate.of(2000, 1, 1));
+    }
 
 
 
