4de31a30c8dfe5bbfdb6afb551a05aef1ac7e9a4
l3miage-ozyonumb <baran.ozyonum@etu.univ-grenoble-alpes.fr>
2024-03-26 21:07:34 +0100

    ess

--- server/src/main/java/fr/uga/l3miage/tp4/repositories/CandidateEvaluationGridRepository.java
+++ server/src/main/java/fr/uga/l3miage/tp4/repositories/CandidateEvaluationGridRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CandidateEvalutionGridRepository extends JpaRepository<CandidateEvaluationGridEntity, Long> {
+public interface CandidateEvaluationGridRepository extends JpaRepository<CandidateEvaluationGridEntity, Long> {
 
 }
