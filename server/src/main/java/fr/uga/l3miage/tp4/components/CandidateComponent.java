e853ac8ebede55f801100ff288c5a9eae3f4fd51
l3miage-fathallm <mariam.fathallah@etu.univ-grenoble-alpes.fr>
2024-03-26 23:32:11 +0100

    requête 4

--- server/src/main/java/fr/uga/l3miage/tp4/components/CandidateComponent.java
+++ server/src/main/java/fr/uga/l3miage/tp4/components/CandidateComponent.java
@@ -1,7 +1,12 @@
 package fr.uga.l3miage.tp4.components;
 
+import fr.uga.l3miage.tp4.models.CandidateEntity;
+import fr.uga.l3miage.tp4.repositories.CandidateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -12,5 +17,9 @@ public class CandidateComponent {
         return null;
     }
     */
+    private final CandidateRepository candidateRepository;
+    public Set<CandidateEntity> getCandidatesWithNoExtraTime(LocalDate dateNaissance){
+        return candidateRepository.findAllByHasExtraTimeFalseAndBirthDateBefore(dateNaissance);
+    }
 
 }
