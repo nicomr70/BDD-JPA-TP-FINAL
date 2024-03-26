9cf2076a02ede275742bb51e0e2e1446b901a698
l3miage-fathallm <mariam.fathallah@etu.univ-grenoble-alpes.fr>
2024-03-26 23:58:38 +0100

    requête 3

--- server/src/main/java/fr/uga/l3miage/tp4/components/CandidateComponent.java
+++ server/src/main/java/fr/uga/l3miage/tp4/components/CandidateComponent.java
@@ -1,20 +1,21 @@
 package fr.uga.l3miage.tp4.components;
 
-import fr.uga.l3miage.tp4.models.CandidateEntity;
+import fr.uga.l3miage.tp4.models.CandidateEvaluationGridEntity;
+import fr.uga.l3miage.tp4.repositories.CandidateEvaluationGridRepository;
 import fr.uga.l3miage.tp4.repositories.CandidateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class CandidateComponent {
 
-    /* TODO à implémenter
-    public Set<CandidateEntity> getAllEliminatedCandidate(){
-        return null;
+    private final CandidateEvaluationGridRepository candidateEvaluationGridRepository;
+    private final CandidateRepository candidateRepository;
+    public Set<CandidateEvaluationGridEntity> getAllEliminatedCandidate(){
+        return candidateEvaluationGridRepository.findAllByGradeIsLessThan(5);
     }
-    */
+
 }
