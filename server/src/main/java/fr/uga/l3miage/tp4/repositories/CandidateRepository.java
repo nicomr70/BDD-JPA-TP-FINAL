ef24c558b18f27d2dc2a93ec9a0ed9c470ae213f
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-03-26 11:50:55 +0100

    Implémentation de la requête 4

--- server/src/main/java/fr/uga/l3miage/tp4/repositories/CandidateRepository.java
+++ server/src/main/java/fr/uga/l3miage/tp4/repositories/CandidateRepository.java
@@ -10,4 +10,5 @@ import java.util.Set;
 @Repository
 public interface CandidateRepository extends JpaRepository<CandidateEntity, Long> {
     Set<CandidateEntity> findAllByTestCenter_Code_Gre();
+    Set<CandidateEntity> findAllByBirthDateBeforeAndHasExtraTimeFalse(LocalDate birthDate);
 }
