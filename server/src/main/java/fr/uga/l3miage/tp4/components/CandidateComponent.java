3ee4b4e426b9177d6247c77ed0b6fb4903e6594e
hounkara <91179744+hounkara@users.noreply.github.com>
2024-03-26 22:21:39 +0100

    1 2 3

--- server/src/main/java/fr/uga/l3miage/tp4/components/CandidateComponent.java
+++ server/src/main/java/fr/uga/l3miage/tp4/components/CandidateComponent.java
@@ -1,5 +1,10 @@
 package fr.uga.l3miage.tp4.components;
 
+import fr.uga.l3miage.tp4.enums.TestCenterCode;
+import fr.uga.l3miage.tp4.models.CandidateEntity;
+import fr.uga.l3miage.tp4.models.CandidateEvaluationGridEntity;
+import fr.uga.l3miage.tp4.repositories.CandidateEvaluationGridRepository;
+import fr.uga.l3miage.tp4.repositories.CandidateRepository;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -7,10 +12,35 @@ import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class CandidateComponent {
 
+
+    private final CandidateRepository candidateRepository;
+    private final CandidateEvaluationGridRepository evaluationGridRepository;
+
+
+    public Set<CandidateEntity> findCandidatsbyTestCenterCode_GR(){
+        return candidateRepository.findAllByTestCenterEntityCode(TestCenterCode.GRE);
+    }
+
+    public Set<CandidateEntity> findCandidatesWithAtLeastOneDisqualifyingGrade() {
+        // Récupère toutes les évaluations éliminatoires
+        Set<CandidateEvaluationGridEntity> disqualifyingEvaluations = evaluationGridRepository.findByGradeLessThanEqual(5);
+
+        // Utilise un Set pour éviter les doublons de candidats
+        Set<CandidateEntity> candidatesWithDisqualifyingGrades = new HashSet<>();
+
+        for (CandidateEvaluationGridEntity evaluation : disqualifyingEvaluations) {
+            // Ajoute le candidat associé à chaque évaluation éliminatoire
+            candidatesWithDisqualifyingGrades.add(evaluation.getCandidateEntity());
+        }
+
+        return candidatesWithDisqualifyingGrades;
+    }
 
 }
--- server/src/main/java/fr/uga/l3miage/tp4/models/CandidateEvaluationGridEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp4/models/CandidateEvaluationGridEntity.java
@@ -3,11 +3,15 @@ package fr.uga.l3miage.tp4.models;
 
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-
+@Getter
+@Setter
 
 public class CandidateEvaluationGridEntity {
     @Id
