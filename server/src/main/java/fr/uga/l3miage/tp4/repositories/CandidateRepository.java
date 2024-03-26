f4289a624ba41f883b08a944a011fa0a8867fb6a
l3miage-gregoirt <teddie.gregoire@etu.univ-grenoble-alpes.fr>
2024-03-26 13:10:32 +0100

    mise a jour tp4

---
+++ server/src/main/java/fr/uga/l3miage/tp4/repositories/CandidateRepository.java
@@ -0,0 +1,11 @@
+package fr.uga.l3miage.tp4.repositories;
+
+import fr.uga.l3miage.tp4.models.CandidateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface CandidateRepository extends JpaRepository<CandidateEntity, Long> {
+}
