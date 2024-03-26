edd196f3ce04867f3167e74d50282548da70cd92
DIALLO Mamadou Hassimiou <mamadou.diallo4@etu.univ-grenoble-alpes.fr>
2024-03-26 18:46:38 +0100

    ajout des repositories

--- server/src/main/java/fr/uga/l3miage/tp4/repositories/CandidateEvaluationGridRepository.java
+++ server/src/main/java/fr/uga/l3miage/tp4/repositories/CandidateEvaluationGridRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CandidatEvaluationGridRepository extends JpaRepository<CandidateEvaluationGridEntity, Long> {
+public interface CandidateEvaluationGridRepository extends JpaRepository<CandidateEvaluationGridEntity, Long> {
 }
